31ce3b43cf090f2841dfb54a9641a2b5c2e54eb4
guojunxu <gjxu@kaikeba.com>
2015-11-16 15:57:56 +0800

    add project

---
+++ VideoPlayerDemo/VideoPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlayerDemo
+//
+//  Created by guojun on 11/16/15.
+//  Copyright © 2015 guojunxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoPlayerDemo/VideoPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoPlayerDemo
+//
+//  Created by guojun on 11/16/15.
+//  Copyright © 2015 guojunxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
